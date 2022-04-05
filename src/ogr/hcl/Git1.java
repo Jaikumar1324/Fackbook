dc78fefa500e3966a198c66d2456dfe471a91c5f
Jaikumar1324 <S.ravijaikumar@gmail.com>
2022-04-05 12:16:17 +0530

    Added Git1

---
+++ src/ogr/hcl/Git1.java
@@ -0,0 +1,19 @@
+package ogr.hcl;
+
+public class Git1 {
+	public void employee10() {
+	      System.out.println("new git");
+		}
+	     public void employee11() {
+	      System.out.println("old git");
+	    }
+	     
+	     public void employee12() {
+	      System.out.println("middle git");
+		}
+	     public void employee13() {
+	       System.out.println("bush git");
+		}
+	    
+	 }
+
